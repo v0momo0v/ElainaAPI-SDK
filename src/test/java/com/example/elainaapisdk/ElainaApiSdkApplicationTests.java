c57ec9c48a4e3c7b0148994f23e9ce7f4e6ae706
Gavin <1524018075@qq.com>
2023-12-12 16:18:28 +0800

    API项目SDK

---
+++ src/test/java/com/example/elainaapisdk/ElainaApiSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elainaapisdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElainaApiSdkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
